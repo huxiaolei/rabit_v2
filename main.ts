25a09e58ccc4096731093ae8fb8ab2a626d04128
Hu Xiaolei (Mike) <huxiaoleimike@gmail.com>
2023-01-05 18:46:06 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,23 +2,38 @@ let distance = 0
 makerbit.connectUltrasonicDistanceSensor(DigitalPin.P12, DigitalPin.P13)
 dfplayermini.connect(SerialPin.P1, SerialPin.P0)
 pins.servoWritePin(AnalogPin.P16, 0)
+pins.servoWritePin(AnalogPin.P15, 0)
+let strip = neopixel.create(DigitalPin.P2, 5, NeoPixelMode.RGB)
+let strip1 = neopixel.create(DigitalPin.P14, 5, NeoPixelMode.RGB)
+let led_colour = 0
+let LED0_strip = strip.range(0, 1)
+let LED0_Strip1 = strip1.range(0, 1)
+strip.showColor(neopixel.hsl(led_colour, 100, 50))
+strip1.showColor(neopixel.hsl(led_colour, 100, 50))
 basic.forever(function () {
-    distance = makerbit.getUltrasonicDistance(DistanceUnit.CM)
     if (distance <= 50) {
-        basic.showIcon(IconNames.Happy)
-        dfplayermini.playFile(0, dfplayermini.isRepeat.No)
-    } else if (distance >= 50 && distance < 100) {
         for (let index = 0; index < 4; index++) {
             basic.showIcon(IconNames.Heart)
             pins.servoWritePin(AnalogPin.P16, 40)
+            pins.servoWritePin(AnalogPin.P15, 40)
             basic.pause(500)
             pins.servoWritePin(AnalogPin.P16, 0)
+            pins.servoWritePin(AnalogPin.P15, 0)
             basic.showIcon(IconNames.SmallHeart)
         }
+    } else if (distance >= 50 && distance < 150) {
+        basic.showIcon(IconNames.Happy)
+        dfplayermini.playFile(0, dfplayermini.isRepeat.No)
+        strip.shift(1)
+        strip1.shift(1)
+        led_colour = led_colour + 10
+        LED0_strip.showColor(neopixel.hsl(led_colour, 100, 50))
+        LED0_Strip1.showColor(neopixel.hsl(led_colour, 100, 50))
     } else {
         basic.showIcon(IconNames.Diamond)
         basic.pause(500)
         basic.showIcon(IconNames.SmallDiamond)
     }
     basic.pause(500)
+    distance = makerbit.getUltrasonicDistance(DistanceUnit.CM)
 })
