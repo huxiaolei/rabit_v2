46e2bddb845e8c95675c2a688aae716c139e97bd
Hu Xiaolei (Mike) <huxiaoleimike@gmail.com>
2023-01-06 15:41:36 +0800

    add one more level to say hello

--- main.ts
+++ main.ts
@@ -10,7 +10,10 @@ let LED0_strip = strip.range(0, 1)
 let LED0_Strip1 = strip1.range(0, 1)
 strip.showColor(neopixel.hsl(led_colour, 100, 50))
 strip1.showColor(neopixel.hsl(led_colour, 100, 50))
+let is_player_on = 0
+let player_counter = 0
 basic.forever(function () {
+    distance = makerbit.getUltrasonicDistance(DistanceUnit.CM)
     if (distance <= 50) {
         for (let index = 0; index < 4; index++) {
             basic.showIcon(IconNames.Heart)
@@ -21,19 +24,28 @@ basic.forever(function () {
             pins.servoWritePin(AnalogPin.P15, 0)
             basic.showIcon(IconNames.SmallHeart)
         }
-    } else if (distance >= 50 && distance < 150) {
+        is_player_on = 0
+    } else if (distance >= 50 && distance < 100) {
         basic.showIcon(IconNames.Happy)
-        dfplayermini.playFile(0, dfplayermini.isRepeat.No)
+        if (is_player_on == 0) {
+            dfplayermini.playFile(1, dfplayermini.isRepeat.No)
+            is_player_on = 1
+        }
         strip.shift(1)
         strip1.shift(1)
         led_colour = led_colour + 10
         LED0_strip.showColor(neopixel.hsl(led_colour, 100, 50))
         LED0_Strip1.showColor(neopixel.hsl(led_colour, 100, 50))
+    } else if (distance >= 100 && distance < 250) {
+        basic.showIcon(IconNames.Asleep)
+        dfplayermini.playFile(2, dfplayermini.isRepeat.No)
+        is_player_on = 0
+        basic.pause(1000)
     } else {
         basic.showIcon(IconNames.Diamond)
         basic.pause(500)
         basic.showIcon(IconNames.SmallDiamond)
+        is_player_on = 0
     }
-    basic.pause(500)
-    distance = makerbit.getUltrasonicDistance(DistanceUnit.CM)
+    basic.pause(100)
 })
